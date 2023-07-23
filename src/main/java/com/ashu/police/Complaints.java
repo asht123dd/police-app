633cecbd908a3d6272849f901397cf39004c054a
Ashutosh <30960101+asht123dd@users.noreply.github.com>
2023-07-23 20:29:15 +0530

    Added user specific complaint retrieval
    Added user specific complaint retrieval

--- src/main/java/com/ashu/police/Complaints.java
+++ src/main/java/com/ashu/police/Complaints.java
@@ -54,6 +54,18 @@ public class Complaints {
 	private String evidence;
 	@Column
 	private String status;
+
+	@Column
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public String getStatus() {
 		return status;
 	}
--- src/main/java/com/ashu/police/PoliceController.java
+++ src/main/java/com/ashu/police/PoliceController.java
@@ -1,6 +1,7 @@
 package com.ashu.police;
 
 import java.net.URI;
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,15 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class PoliceController {
-	
+
 	@Autowired
 	PoliceService policeService;
 
@@ -25,18 +30,20 @@ public class PoliceController {
 		List<String> strings = Arrays.asList("string1", "string2", "string3");
 		return ResponseEntity.ok(strings);
 	}
-	
+
 	@GetMapping("/complaints")
 	@PreAuthorize("hasRole('OFFICER')")
 	public ResponseEntity<List<Complaints>> getComplaints() {
 		return ResponseEntity.ok(policeService.getAllComplaints());
 	}
-	
+
 	@PostMapping("/complaints")
 	public ResponseEntity<Complaints> makeComplaint(@RequestBody Complaints complaint) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		complaint.setStatus("FILED");
+		complaint.setUsername(authentication.getName());
 		policeService.saveOrUpdate(complaint);
-		return new ResponseEntity<Complaints>(complaint,HttpStatus.CREATED);
+		return new ResponseEntity<Complaints>(complaint, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/complaints/{complaintId}")
@@ -45,10 +52,17 @@ public class PoliceController {
 
 		boolean hasUserRole = authentication.getAuthorities().stream()
 				.anyMatch(r -> r.getAuthority().equals("ROLE_OFFICER"));
+		System.out.println(authentication.getName());
+		String user = authentication.getName();
 		if (hasUserRole) {
 			return new ResponseEntity<Complaints>(policeService.getComplaintById(complaintId), HttpStatus.OK);
 		} else {
-			return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
+			Complaints complaint = policeService.getComplaintById(complaintId);
+			if (user.equals(complaint.getUsername())) {
+				return new ResponseEntity<Complaints>(complaint, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>("Unauthorized", HttpStatus.UNAUTHORIZED);
+			}
 		}
 	}
 }
